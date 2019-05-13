dd8ddb610e33b12216de241d2d0866666ba00273
shouzimu <intelcorecpu@gmail.com>
2019-05-13 19:34:33 +0800

    discovery重设为Eureka

--- server-discovery/src/main/java/com/dh/cloud/DiscoveryApplication.java
+++ server-discovery/src/main/java/com/dh/cloud/DiscoveryApplication.java
@@ -1,13 +1,15 @@
 package com.dh.cloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class ZkDiscoveryServerApplication {
+@EnableEurekaServer
+public class DiscoveryApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(ZkDiscoveryServerApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
 }
